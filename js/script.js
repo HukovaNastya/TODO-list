66ed53c4243e614e4b8739dd3b65b2e936fc6751
HukovaNastya <hukovanasta@gmail.com>
2025-10-13 12:35:35 +0300

    added-initial-todo-fn-with-todo-structure

---
+++ js/script.js
@@ -0,0 +1,33 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+//Functions
+function addTodo(e){
+    e.preventDefault();
+    console.log(e);
+    //Create TODO div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    //Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText ="Hey";
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+    //Create check mark button
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("completed-btn");
+    todoDiv.appendChild(completedButton);
+    //Create trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    //Append todo to the todoList(ul)
+    todoDiv.appendChild(trashButton);
+    todoList.appendChild(todoDiv);
+}
